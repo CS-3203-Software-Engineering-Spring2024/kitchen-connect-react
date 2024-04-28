1bbe995dc298369d9e724f636cc5dcc25f09f57c
JesseC-R <jesse.t.crauthers-1@ou.edu>
2024-04-27 21:16:06 -0500

    added more posts using useState array

--- src/pages/feed/Feed.tsx
+++ src/pages/feed/Feed.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './feed.css';
 
 import {
@@ -8,61 +8,72 @@ import {
     repostIcon,
     bookmarkIcon,
     gordonRamsey,
+    burgerWithLettuce,
 } from '../../assets';
 
 // TODO: Change photos to not use Gordan Ramsey as example
 const Feed = () => {
+    const [posts, setPosts] = useState([
+        { picture: gordonRamsey, content: anhnguyen, body: "test test test", author: "Gordan", id: 1 },
+        { picture: gordonRamsey, content: burgerWithLettuce, body: "test test2 test2", author: "Jon", id: 2 },
+        { picture: gordonRamsey, content: anhnguyen, body: "test test3 test3", author: "Gary", id: 3 },
+        { picture: gordonRamsey, content: anhnguyen, body: "test test4 test4", author: "Billy", id: 4 },
+        { picture: gordonRamsey, content: anhnguyen, body: "test test4 test4", author: "Karen", id: 5 }
+    ]);
+// TODO: Add comment functionality on posts
+    //const [comments, setComments] = useState([]);
+
     return (
-        <div className="feed">
-            {/* Sample post */}
-            <div className="post">
-                {/* User info */}
-                <div className="user-info">
-                    <img
-                        className="user-photo"
-                        src={gordonRamsey} // Replace with actual user photo URL or use a placeholder
-                        alt="User"
-                    />
-                    <span className="username">gordonramsey</span>
-                </div>
-                {/* Post content */}
-                <div className="post-content">
-                    <img
-                        className="post-photo"
-                        src={anhnguyen} // Replace with actual post photo URL or use a placeholder
-                        alt="Photo by Anh Nguyen on Unsplash"
-                    />
-                    <div className="post-actions">
-                        {/* Like button */}
-                        <button className="post-actions-button like-button">
-                            <img src={likeIcon} alt="Like" />
-                        </button>
-                        {/* Comment button */}
-                        <button className="post-actions-button comment-button">
-                            <img src={commentIcon} alt="Comment" />
-                        </button>
-                        {/* Repost button */}
-                        <button className="post-actions-button repost-button">
-                            <img src={repostIcon} alt="Repost" />
-                        </button>
-                        <button className="post-actions-button save-button">
-                            <img src={bookmarkIcon} alt="Save" />
-                        </button>
+        <div className="feed-container">
+            <div className="feed">
+                {posts.map((post) => (
+                    <div className="post" key={post.id}>
+                        {/* User info */}
+                        <div className="user-info">
+                            <img
+                                className="user-photo"
+                                src={post.picture} // Use post picture instead of gordonRamsey
+                                alt="User"
+                            />
+                            <span className="username">{post.author}</span>
+                        </div>
+                        {/* Post content */}
+                        <div className="post-content">
+                            <img
+                                className="post-photo"
+                                src={post.content} // Use placeholder image or actual post photo URL
+                                alt="Photo by Anh Nguyen on Unsplash"
+                            />
+                            <div className="post-actions">
+                                {/* Like button */}
+                                <button className="post-actions-button like-button">
+                                    <img src={likeIcon} alt="Like" />
+                                </button>
+                                {/* Comment button */}
+                                <button className="post-actions-button comment-button">
+                                    <img src={commentIcon} alt="Comment" />
+                                </button>
+                                {/* Repost button */}
+                                <button className="post-actions-button repost-button">
+                                    <img src={repostIcon} alt="Repost" />
+                                </button>
+                                <button className="post-actions-button save-button">
+                                    <img src={bookmarkIcon} alt="Save" />
+                                </button>
+                            </div>
+                            <div className="caption-container">
+                                {/* Post caption */}
+                                <p className="caption">
+                                    <span className="caption-username">
+                                        {post.author}
+                                    </span>{' '}
+                                    {post.body}
+                                </p>
+                            </div>
+                        </div>
                     </div>
-                    <div className="caption-container">
-                        {/* Post caption */}
-                        <p className="caption">
-                            <span className="caption-username">
-                                gordonramsey
-                            </span>{' '}
-                            Oh boy! Look at this beautiful dish! This is my
-                            first post on KitchenConnect, this app is going to
-                            be great! #foodie #idiotsandwich
-                        </p>
-                    </div>
-                </div>
+                ))}
             </div>
-            {/* Add more posts here */}
         </div>
     );
 };
