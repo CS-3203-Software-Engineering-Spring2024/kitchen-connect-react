f701077435243903d78ae43dfc66989d7f262c78
hai-henry <hvnryho@gmail.com>
2024-04-28 16:44:03 -0500

    Created Grid system

--- src/pages/Recipe/Recipe.tsx
+++ src/pages/Recipe/Recipe.tsx
@@ -1,11 +1,35 @@
-// RecipesPage.tsx
 import React from 'react';
+import { anhnguyen, burgerWithLettuce, whiteDimSum } from '../../assets';
+import './recipe.css';
+
+interface Recipe {
+    name: string;
+    image: string;
+}
+
+const recipes: Recipe[] = [
+    // Add your recipes here
+    { name: 'Tofu Bowl', image: anhnguyen },
+    { name: 'Cheeseburger', image: burgerWithLettuce },
+    { name: 'Dumpling', image: whiteDimSum },
+];
 
 const Recipe: React.FC = () => {
     return (
         <div>
-            <h1>Recipes Page</h1>
-            <p>This is the Recipes page.</p>
+            <h1>Saved Recipes</h1>
+            <div className="recipe-grid">
+                {recipes.map((recipe, index) => (
+                    <div key={index} className="recipe-card">
+                        <img
+                            src={recipe.image}
+                            alt={recipe.name}
+                            className="recipe-image"
+                        />
+                        <p className="recipe-name">{recipe.name}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
