1d74920bb2e2d2921db22fafccb9ab04739d006f
JesseC-R <jesse.t.crauthers-1@ou.edu>
2024-04-30 13:06:52 -0500

    exported comments

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Feed } from './feed/Feed';
 export { default as Recipe } from './Recipe/Recipe';
 export { default as Profile } from './profile/Profile';
+export { default as Comments} from './Comments/Comments'
