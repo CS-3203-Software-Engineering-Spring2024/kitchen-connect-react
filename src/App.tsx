34c53f3ca9c2dab00a8174856bc070a0f1c7fb97
Henry Ho <hvnryho@gmail.com>
2024-04-29 23:55:00 -0500

    Fixed scrolling issue

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,9 @@ function App() {
 
     return (
         <div className="app">
+            <div className="app-welcome">
+                <WelcomePage />
+            </div>
             <div className="app-layout">
                 {!authenticated ? (
                     <div className="app-layout-sign-in">
