47d59bfb452c82321193694daf1a633af8269d5b
hai-henry <henry.h.ho-1@ou.edu>
2024-04-30 12:00:23 -0500

    Recipe now goes to Recipe page but photo too big

--- src/pages/Recipe/Recipe.tsx
+++ src/pages/Recipe/Recipe.tsx
@@ -1,72 +1,22 @@
 import React from 'react';
-import { anhnguyen, burgerWithLettuce, whiteDimSum } from '../../assets';
-import { addicon } from '../../assets/index';
-import './recipe.css';
 
-interface Collection {
-    name: string;
-    image: string;
+interface RecipeProps {
+    recipes: { image: File; name: string; tags: string[] }[];
 }
 
-interface Recipe {
-    name: string;
-    image: string;
-}
-
-const groupedRecipes: Collection[] = [
-    { name: 'Vegetarian', image: anhnguyen },
-    { name: 'Burgers', image: burgerWithLettuce },
-    { name: 'Dim Sum', image: whiteDimSum },
-];
-
-const ungroupedRecipes: Recipe[] = [
-    { name: 'Tofu bowl', image: anhnguyen },
-    { name: 'Tofu bowl', image: anhnguyen },
-    { name: 'Tofu bowl', image: anhnguyen },
-    { name: 'Cheeseburger', image: burgerWithLettuce },
-    { name: 'Simple white dumplings', image: whiteDimSum },
-];
-
-const Recipe: React.FC = () => {
+const Recipe: React.FC<RecipeProps> = ({ recipes }) => {
     return (
-        <div className="recipe-page-container">
-            <div className="title-container">
-                <h2 className="recipe-page-container-title">Collections</h2>
-                <button className="collection-add-button">
-                    <img src={addicon} alt="Add sign" />
-                </button>
-            </div>
-            <div className="recipe-grid">
-                {groupedRecipes.map((collection, index) => (
+        <div className="recipe-grid">
+            {recipes.map((recipe, index) => {
+                const imageUrl = URL.createObjectURL(recipe.image);
+                return (
                     <div key={index} className="recipe-card">
-                        <div className="recipe-card-inner">
-                            <img
-                                src={collection.image}
-                                alt={collection.name}
-                                className="recipe-image"
-                            />
-                            <p className="recipe-name collection-name">
-                                {collection.name}
-                            </p>
-                        </div>
+                        <img src={imageUrl} alt={recipe.name} />
+                        <h2>{recipe.name}</h2>
+                        <p>{recipe.tags.join(', ')}</p>
                     </div>
-                ))}
-            </div>
-            <h2 className="recipe-page-container-title">Recipes</h2>
-            <div className="recipe-grid">
-                {ungroupedRecipes.map((recipe, index) => (
-                    <div key={index} className="recipe-card">
-                        <div className="recipe-card-inner">
-                            <img
-                                src={recipe.image}
-                                alt={recipe.name}
-                                className="recipe-image"
-                            />
-                            <p className="recipe-name">{recipe.name}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
+                );
+            })}
         </div>
     );
 };
